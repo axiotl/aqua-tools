1a65ca77c66bde1289fb7f44bb65569042b65b2f
Maharshi Chakraborty <maharshichakravortee@gmail.com>
2022-08-23 18:01:28 +0200

    updating to latest version

--- annotate_loops.sh
+++ annotate_loops.sh
@@ -2,10 +2,32 @@
 
 data_dir=~/lab-data
 aqua_dir=~/aqua_tools
+ram_disk=/media/ram_disk
+
+
+ctrlc_count=0
+
+function no_sigint {
+
+    let ctrlc_count++
+    if [[ $ctrlc_count == 1 ]]; then
+        echo "Aborting process"
+        if [ -d "$ram_disk" ]; then
+        sudo umount "$ram_disk"
+        cd /media
+        sudo rm -r ram_disk
+        else
+        :
+        fi
+    fi
+
+}
+
+trap no_sigint SIGINT
 
 function usage {
     echo -e "usage: "
-    echo -e "  annotate_pairs.sh \\"
+    echo -e "  annotate_loops.sh \\"
     echo -e "    -G GENOME_BUILD \\"
     echo -e "    -P PATH_TO_BEDPE_YOU_WANT_TO_ANNOTATE \\"
     echo -e "    -A NAME_OF_FIRST_SAMPLE \\"
@@ -24,23 +46,27 @@ function help {
     echo "---------------"
     echo "OPTIONS"
     echo
-    echo "   -P|--pair           PATH_TO_GENOMIC_PAIRS_FILE  : Full path to the bedpe (pairs) file you want to annotate. If bedpe contains headers, see manual on how to remove them!"
-    echo "   -A|--sample1        NAME_OF_FIRST_SAMPLE        : Name of the sample you want to use the AQuA normalized values to annotate it with, as it appears on the Tinkerbox"
-    echo "   -G|--genome         GENOME_BUILD                : The genome build the sample(s) has been processed using. Strictly hg19 or hg38"
-    echo "  [-Q|--AQuA       ]   USE_AQUA_FACTORS            : TRUE or FALSE"
-    echo "  [-B|--sample2    ]   NAME_OF_SECOND_SAMPLE       : The name of the second sample. If triggered, calculates the delta AQuA normalized values from both samples for that pair. Useful in case vs control."
-    echo "  [-R|--resolution ]   RESOLUTION                  : Resolution of sample in basepairs, using which the contact values should be calculated. Default 5000. Accepted resolutions- 1000,5000,10000,25000,50000,100000,250000,500000,1000000,2500000"
-    echo "  [-h|--help       ]   Help message"
+    echo "   -P|--pair            PATH_TO_GENOMIC_PAIRS_FILE  : Full path to the bedpe (pairs) file you want to annotate, without headers! "
+    echo "   -A|--sample1         NAME_OF_FIRST_SAMPLE        : Name of the sample you want to use as it appears on the Tinkerbox"
+    echo "   -G|--genome          GENOME_BUILD                : The genome build the sample(s) has been processed using. Strictly hg19 or hg38"
+    echo "  [-Q|--norm        ]   NORMALIZATION               : Which normalization to use. Strictly 'none', 'cpm' or 'aqua' in lower case"
+    echo "  [-B|--sample2     ]   NAME_OF_SECOND_SAMPLE       : The name of the second sample. If triggered, calculates the delta contact values for that pair. Useful in case vs control"
+    echo "  [-R|--resolution  ]   RESOLUTION                  : Resolution of sample in basepairs, using which the contact values should be calculated. Default 5000. Accepted resolutions- 1000,5000,10000,25000,50000,100000,250000,500000,1000000,2500000"
+    echo "  [-f|--formula     ]   FORMULA                     : Arithmetic to use to report contact calues. Options- center, max, average, sum. Default = center"
+    echo "  [-F|--fix         ]   FIXED_COORDINATES           : If FALSE, reports new coordinates based on arithmetic center or max. Default = TRUE"
+    echo "  [   --shrink_wrap ]   SHRINK_WRAP                 : Squeezes a 2D bedpe interval until supplied value(in raw read count units) is reached. Default = FALSE"
+    echo "  [   --split       ]   SPLIT                       : Splits a 2D bedpe interval into multiple sub-intervals greater than supplied value(in raw read count units). Default = FALSE"
+    echo "  [   --padding     ]   PADDING                     : Joins sub-intervals in 2D space reported by --split, based on supplied value(in bin units). Default = 2"
+    echo "  [   --expand      ]   EXPAND                      : Expands 1D bedpe feet in both directions based on supplied value(in bin units). Default = 0"
+    echo "  [-h|--help        ]   Help message"
     exit;
 }
-
 
 if [ $# -lt 1 ]
     then
     usage
     exit
 fi
-
 
 # Transform long options to short ones
 for arg in "$@"; do
@@ -49,17 +75,31 @@ for arg in "$@"; do
       "--pair")         set -- "$@" "-P" ;;
       "--sample1")      set -- "$@" "-A" ;;
       "--genome")       set -- "$@" "-G" ;;
-      "--AQuA")         set -- "$@" "-Q" ;;
+      "--norm")         set -- "$@" "-Q" ;;
       "--sample2")      set -- "$@" "-B" ;;
-      "--resolution")   set -- "$@" "-R" ;;  
+      "--resolution")   set -- "$@" "-R" ;;
+      "--formula")      set -- "$@" "-f" ;;
+      "--fix")          set -- "$@" "-F" ;;
+      "--split")        set -- "$@" "-S" ;; 
+      "--shrink_wrap")  set -- "$@" "-s" ;;
+      "--padding")      set -- "$@" "-p" ;;
+      "--expand")       set -- "$@" "-e" ;;  
       "--help")         set -- "$@" "-h" ;;
        *)               set -- "$@" "$arg"
   esac
 done
 
-R=5000
 
-while getopts ":P:A:G:Q:B:R:h" OPT
+Q=none
+R=5000
+f=center
+F=TRUE
+S=FALSE
+s=FALSE
+p=2
+e=0
+
+while getopts ":P:A:G:Q:B:R:f:F:S:s:p:e:h" OPT
 do
     case $OPT in
   P) P=$OPTARG;;
@@ -68,6 +108,12 @@ do
   Q) Q=$OPTARG;;
   B) B=$OPTARG;;
   R) R=$OPTARG;;
+  f) f=$OPTARG;;
+  F) F=$OPTARG;;
+  S) S=$OPTARG;;
+  s) s=$OPTARG;;
+  p) p=$OPTARG;;
+  e) e=$OPTARG;;
   h) help ;;
   \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -83,6 +129,8 @@ do
 done
 
 
+
+# If no sample B, the variable is empty:
 if [ -z "$B" ]; then
   case $B in
   B) B="";;
@@ -90,14 +138,16 @@ if [ -z "$B" ]; then
 fi
 
 
+# Do all necessary parameter checks
+#----------------------------------
+
 if [[ -z $P ]];
 then
     usage
     exit
 fi
 
-if [[ -z $Q ]]; then Q="TRUE"; fi
-
+#----------------------------------
 
 
 col1=$(head -n 1  $P | cut -f 1 )
@@ -111,46 +161,119 @@ if [[ $col1 != $col4  ]]; then
   exit
 fi
 
-if [[ -z $A ]];
+#----------------------------------
+
+if [[ -z $A ]]
 then
     usage
     exit
 fi
 
-if [[ -z $G ]];
+#----------------------------------
+
+if [[ -z $G ]]
 then
     usage
     exit
 fi
 
+
+num_loops=`cat "$P" | wc -l`
+
+
+
+# RAM-disk update
+if [ ! -d "$ram_disk" ]; then
+ ## Proceeding with RAM_disk annotate_loops
+ :
+else
+  echo "RAM disk currently in use on this machine. Please wait for the other process to stop."
+  exit
+fi
+
+sudo mkdir -p "$ram_disk"
+
+buffer=100000000
+
+###########################################################################
+###########################################################################
+###                                                                     ###
+###                         ONE-SAMPLE ANALYSIS                         ###
+###                                                                     ###
+###########################################################################
+###########################################################################
 
 if [ -z "$B" ]
 then
+    
+    size_file=`du -b $data_dir/$G/$A/$A.allValidPairs.hic | cut -f 1`
+    size_disk=`echo "$size_file+$buffer" | bc`
 
-#echo "Commencing one sample analysis..."
-Rscript \
-$aqua_dir/annotate_loops.r \
-  $P \
-  $R \
-  $data_dir/$G/$A/$A.allValidPairs.hic \
-  $data_dir/$G/$A/mergeStats.txt \
-  $Q
+    sudo mount -t tmpfs -o size="$size_disk" tmpfs "$ram_disk"
+
+    ## echo "RAM disk successfully created and mounted"
+
+    sudo cp $data_dir/$G/$A/$A.allValidPairs.hic /media/ram_disk
+
+    ## echo ".hic copied to RAM disk"
+
+    Rscript \
+    $aqua_dir/annotate_loops.r \
+      $P \
+      $R \
+      $ram_disk/$A.allValidPairs.hic \
+      $data_dir/$G/$A/mergeStats.txt \
+      $Q \
+      $G \
+      $num_loops \
+      $f $F $S $s $p $e
 fi
+
+
+
+###########################################################################
+###########################################################################
+###                                                                     ###
+###                         TWO-SAMPLE ANALYSIS                         ###
+###                                                                     ###
+###########################################################################
+###########################################################################
 
 if [ -n "$B" ]
-then 
+then
 
-#echo "Commencing two sample analysis..."  
+    size_file1=`du -b $data_dir/$G/$A/$A.allValidPairs.hic | cut -f 1`
+    size_file2=`du -b $data_dir/$G/$B/$B.allValidPairs.hic | cut -f 1`
+    size_disk=`echo "$size_file1+$size_file2+$buffer" | bc`
 
-Rscript \
-$aqua_dir/annotate_loops.r \
-  $P \
-  $R \
-  $data_dir/$G/$A/$A.allValidPairs.hic \
-  $data_dir/$G/$A/mergeStats.txt \
-  $data_dir/$G/$B/$B.allValidPairs.hic \
-  $data_dir/$G/$B/mergeStats.txt \
-  $Q
+    sudo mount -t tmpfs -o size="$size_disk" tmpfs "$ram_disk"
+
+    ## echo "RAM disk successfully created and mounted"
+
+    sudo cp $data_dir/$G/$A/$A.allValidPairs.hic /media/ram_disk
+    sudo cp $data_dir/$G/$B/$B.allValidPairs.hic /media/ram_disk
+
+    ## echo ".hics copied to RAM disk"
+
+    Rscript \
+    $aqua_dir/annotate_loops.r \
+      $P \
+      $R \
+      $ram_disk/$A.allValidPairs.hic \
+      $data_dir/$G/$A/mergeStats.txt \
+      $ram_disk/$B.allValidPairs.hic \
+      $data_dir/$G/$B/mergeStats.txt \
+      $Q \
+      $G \
+      $num_loops \
+      $f $F $S $s $p $e
 fi
 
 
+if [[ $ctrlc_count == 0 ]]; then
+    sudo umount "$ram_disk"
+    cd /media
+    sudo rm -r ram_disk
+fi
+
+## echo "RAM disk unmounted and removed"
